52fcb64df54140fb765dcd919e28ef620cdb1652
INTERN/Man378 <alina.grohe@students.hs-mainz.de>
2022-11-11 12:51:39 +0100

    for schliefe für dice eingesetzt mit arrays

--- Assets/Scripts/102 Prototype/DiceRoll.cs
+++ Assets/Scripts/102 Prototype/DiceRoll.cs
@@ -8,38 +8,57 @@ public class DiceRoll : MonoBehaviour
         public AudioSource audioSource;
         public string message = "It's a Win";
         
-        public int lucyNumber5 = 5;
-        public int lucyNumber1 = 1;
-        public int lucyNumber4 = 4;
+        // public int lucyNumber5 = 5;
+        // public int lucyNumber1 = 1;
+        // public int lucyNumber4 = 4;
 
-    
+    public int [] luckyNumbers = {1,4,5};
        
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //luckyNumbers [0] = 1; 
+        //luckyNumbers [1] = 4;
+        //luckyNumbers [2] = 5;
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
         if (Input.GetKeyDown("w"))
         {
             diceNumber = Random.Range(1,7);
-            Debug.Log("You rolled number: " + diceNumber);
-            
-            // Ausführung durch Oder zeichen in If statement 
+            //Debug.Log("You rolled number: " + diceNumber);
 
-            if (diceNumber == lucyNumber1 || diceNumber == lucyNumber5 || diceNumber == lucyNumber4)
+              // Ausführung mit Array durch forschleife 
+            
+            Debug.Log("You Rolled a " + diceNumber);
+
+            for (int i = 0; i < luckyNumbers.Length; i++)
             {
-                 audioSource.Play();
-                Debug.Log("<color=green> Lucy Number </color> "+ diceNumber  +" "+ message );
+                Debug.Log("for loop i" + i);
+                if (diceNumber == luckyNumbers[i])
+                {
+                     audioSource.Play();
+                    Debug.Log("<color=green> Lucy Number </color> "+ diceNumber  +" "+ message );
+                }
+                else
+                { 
+                Debug.Log("<color=red> You Lose </color>");
+                }
             }
-            else 
-            {
-            Debug.Log("<color=red> You Lose </color>");
-            }
+            // // Ausführung durch Oder zeichen in If statement 
+
+            // if (diceNumber == lucyNumber1 || diceNumber == lucyNumber5 || diceNumber == lucyNumber4)
+            // {
+            //      audioSource.Play();
+            //     Debug.Log("<color=green> Lucy Number </color> "+ diceNumber  +" "+ message );
+            // }
+            // else 
+            // {
+            // Debug.Log("<color=red> You Lose </color>");
+            //}
         //     // Lucy Number 5
         //     if (diceNumber == 5) 
         //     {
