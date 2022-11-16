9cdaddc4137be9c29e47da50a6df498c11846c76
Saltinvolved <alina.grohe@live.de>
2022-11-16 18:34:04 +0100

    Dice Roll auf 50 mit 8 lucy Numbers

--- Assets/Scripts/102 Prototype/DiceRoll.cs
+++ Assets/Scripts/102 Prototype/DiceRoll.cs
@@ -7,20 +7,13 @@ public class DiceRoll : MonoBehaviour
         public int diceNumber;
         public AudioSource audioSource;
         public string message = "It's a Win";
-        
-        // public int lucyNumber5 = 5;
-        // public int lucyNumber1 = 1;
-        // public int lucyNumber4 = 4;
-
-    public int [] luckyNumbers = {1,4,5};
+        public int [] luckyNumbers = {22,8,13,50,48,31,32,27};
        
 
     // Start is called before the first frame update
     void Start()
     {
-        //luckyNumbers [0] = 1; 
-        //luckyNumbers [1] = 4;
-        //luckyNumbers [2] = 5;
+       
     }
 
     // Update is called once per frame
@@ -28,56 +21,23 @@ public class DiceRoll : MonoBehaviour
     {
         if (Input.GetKeyDown("w"))
         {
-            diceNumber = Random.Range(1,7);
-            //Debug.Log("You rolled number: " + diceNumber);
-
-              // Ausführung mit Array durch forschleife 
-            
+            diceNumber = Random.Range(1,51);
+            // Ausführung mit Array durch forschleife 
             Debug.Log("You Rolled a " + diceNumber);
-
             for (int i = 0; i < luckyNumbers.Length; i++)
             {
                 Debug.Log("for loop i" + i);
                 if (diceNumber == luckyNumbers[i])
                 {
-                     audioSource.Play();
+                    audioSource.Play();
                     Debug.Log("<color=green> Lucy Number </color> "+ diceNumber  +" "+ message );
                 }
                 else
                 { 
-                Debug.Log("<color=red> You Lose </color>");
+                    Debug.Log("<color=red> You Lose </color>");
                 }
             }
-            // // Ausführung durch Oder zeichen in If statement 
-
-            // if (diceNumber == lucyNumber1 || diceNumber == lucyNumber5 || diceNumber == lucyNumber4)
-            // {
-            //      audioSource.Play();
-            //     Debug.Log("<color=green> Lucy Number </color> "+ diceNumber  +" "+ message );
-            // }
-            // else 
-            // {
-            // Debug.Log("<color=red> You Lose </color>");
-            //}
-        //     // Lucy Number 5
-        //     if (diceNumber == 5) 
-        //     {
-        //         audioSource.Play();
-        //         Debug.Log( "<color=red> Lucy Number </color> "+ diceNumber  +" "+ message );
-        //     }
-        //     // Lucy Number 1
-        //     if (diceNumber == 1) 
-        //     {
-        //         audioSource.Play();
-        //         Debug.Log( "<color=green> Lucy Number </color> "+ diceNumber  +" "+ message );
-        //     }
-        //     // Lucy Number 4 
-        //       if (diceNumber == 4) 
-        //     {
-        //         audioSource.Play();
-        //         Debug.Log( "<color=blue> Lucy Number </color> "+ diceNumber  +" "+ message );
-        //     }
-        // 
+     
         }
     
     }
