4b6a386e2f3e7074c9c189865656d71d3ec16d5a
Saltinvolved <alina.grohe@live.de>
2022-11-24 16:13:59 +0100

    Fix Particles effects on goal but Decoy prefabs dissapeared (fix needed)

--- Assets/Scripts/Prototype 101/TriggerGoal.cs
+++ Assets/Scripts/Prototype 101/TriggerGoal.cs
@@ -6,34 +6,60 @@ public class TriggerGoal : MonoBehaviour
 {
     public GameObject Roboter;
     public AudioSource audioSource;
-    public ParticleSystem collisionParticleSystem;
-    
-
-   // public GameObject Boom;
+    public ParticleSystem playParticleSystem;
+    public ParticleSystem emitParticleSystem;
     
     
  
     
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(gameObject.name + "just hit" + other.name);
+        Debug.Log(gameObject.name + "triggerd into" + gameObject.name);
 
+         // When roboter collides with goal 
         if(other.name == Roboter.name)
         { 
-            // When roboter collides with goal 
-            
             audioSource.Play();
             Debug.Log("Victory");
-            var em = collisionParticleSystem.emission;
+
+            //Using a ParticleSystem for emission 
+            EmitParticles();
            
-
-            em.enabled = true;
-            collisionParticleSystem.Play();
-
-            
-            
-            
+           // Using a ParticleSystem with play and Stop - play true
+        
+            PlayParticles(true);
         }
     }
-   
+    
+    private void OnTriggerExit(Collider other)
+    {
+
+        Debug.Log(other.name + "triggered into" + gameObject.name);
+
+        //when roboter collides with goal 
+        if(other.name == Roboter.name)
+        {
+            Debug.Log("Exit");
+            //using a ParticleSystem with Play and Stop - play false 
+            PlayParticles(false); 
+        }
+    }
+
+    //custom method to execute emitting of partiles 
+    void EmitParticles()
+    {
+        emitParticleSystem.Emit(50);
+    }
+    void PlayParticles(bool on)
+    {
+        if(on)
+        {
+            playParticleSystem.Play();
+        }
+        else if(!on)
+        {
+            playParticleSystem.Stop();
+        }
+    }
+
 }
