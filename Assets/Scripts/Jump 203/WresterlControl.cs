69ed73570c079023229a99988058ca82e1cdadbf
Saltinvolved <alina.grohe@live.de>
2022-11-26 14:47:52 +0100

    Eigene Übungen Charater und Animations Import

---
+++ Assets/Scripts/Jump 203/WresterlControl.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WresterlControl : MonoBehaviour
+{
+      public float horizontalInput;
+    public float forwardInput;
+    public float speed;
+    public float turnSpeed;
+    public Vector3 force;
+    public GameObject Plane; 
+
+    private Animator _playerAnim;
+    private Rigidbody _playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerAnim = GetComponent<Animator>();
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+
+        _playerAnim.SetFloat("Run", forwardInput);
+
+        if (forwardInput != 0 || horizontalInput != 0)
+        {
+            _playerAnim.SetBool("Walk", true);
+        }
+        else
+        {
+            _playerAnim.SetBool("Walk", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRb.AddForce(force, ForceMode.Impulse);
+            _playerAnim.SetTrigger("Jump");
+        }
+    }
+
+    // Grond Check for Jump? 
+     private void OnTriggerEnter(Collider other)
+    {
+
+         // When player touches the Ground  
+        if(other.name == Plane.name)
+        { 
+            
+            Debug.Log("Player ist touching the ground");
+
+            
+        }
+    }        
+}
