6ac3917ffa2910c25029b33edad25e9eaa2b0941
INTERN/Man378 <alina.grohe@students.hs-mainz.de>
2022-12-09 14:05:23 +0100

    Versuch Jump zu ajusten_ muss korrigert werden_bin nicht mitgekommen _ jumtime und cancel funktioniert nicht _ artime add hat nicht fuktioniert. amcht jetzt immer kurzen jump

--- Assets/Scripts/Jump 203/CameraFollow.cs
+++ Assets/Scripts/Jump 203/CameraFollow.cs
@@ -6,10 +6,17 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset;
+    public Transform target;
+
+    void Start ()
+    {
+        //target = player.transform;
+    }
 
     
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+        transform.LookAt(target);
     }
 }
--- Assets/Scripts/Jump 203/WresterlControl.cs
+++ Assets/Scripts/Jump 203/WresterlControl.cs
@@ -13,12 +13,17 @@ public class WresterlControl : MonoBehaviour
     public GameObject Plane; 
     public GameObject Obsticle;
     public float gravityModifier;
-
+    public float jumpTimer;
+    public float jumpButtonPressTime =2;
+    
     private Animator _playerAnim;
     private Rigidbody _playerRb;
+   
+    public bool jumpCancelled;
     public bool isJumping;
     public bool isGrounded;
     public bool isFalling;
+    public bool isLanding;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,12 +44,7 @@ public class WresterlControl : MonoBehaviour
         transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
         transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
  
-    // Ground Test 
-       if (isGrounded)
-       {
-       Debug.Log("Player ist touching the ground"); 
-       }   
-    // Wrestler is Walkin and Running
+        // Wrestler is Walkin and Running
         _playerAnim.SetFloat("Run", forwardInput);
 
         if (forwardInput != 0 || horizontalInput != 0)
@@ -56,7 +56,7 @@ public class WresterlControl : MonoBehaviour
             _playerAnim.SetBool("IsMoving", false);
         }
     // is Jumping  
-        if((Input.GetKeyDown(KeyCode.Space)) && (isGrounded))
+        if((Input.GetKeyDown(KeyCode.Space)) && (isGrounded) && !isFalling)
         {
             isJumping = true;
             isGrounded = false;
@@ -70,14 +70,47 @@ public class WresterlControl : MonoBehaviour
         {
             isJumping = false;
             isFalling = true;
+            
            
             if(isFalling)
             {
                 _playerAnim.SetBool("IsFalling",true);
-
-            //_playerAnim.SetBool("IsFalling",true);
+                //_playerAnim.SetBool("IsGrounded",false);
             }
-           
+
+            if(isJumping)
+            {
+                jumpTimer += Time.deltaTime;
+                
+                if(Input.GetKeyUp(KeyCode.Space))
+                {
+                    jumpCancelled =true;
+
+                    isJumping = false;
+                    isFalling = true;
+                } 
+                if(jumpTimer > jumpButtonPressTime)
+                {
+                    isJumping = false;
+                }
+            }
+            if (_playerRb.velocity.y < 0 && isFalling)
+            {
+                isLanding = true;
+                isFalling = false;
+                _playerAnim.SetBool("IsFalling",false); 
+            }
+            // Backwards 
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+             _playerAnim.SetTrigger("Backwards");
+            }
+
+            // // Kick
+            // if(Input.GetKeyDown (KeyCode.Q))
+            // {
+            // _PlayerAnim.SetTrigger("Kick");
+            // }
         }
     
     }
@@ -85,14 +118,16 @@ void FixedUpdate()
     {
         if(isJumping)
         {
+            gravityModifier = 1f;
             _playerRb.AddForce(Vector3.up*force, ForceMode.Force);
             
         }
-        if(isFalling || isGrounded)
+        if(isFalling || isGrounded || isLanding || jumpCancelled)
         {
-            _playerRb.AddForce(Vector3.down * forceDown * _playerRb.mass);
+           // _playerRb.AddForce(Vector3.down * forceDown * _playerRb.mass);
+             gravityModifier = 25f;
         }
-        
+        _playerRb.AddForce(Physics.gravity * (gravityModifier -1) * _playerRb.mass);
 }
     // // Grond Check for Jump? 
     //  private void OnTriggerEnter(Collider other)
@@ -113,10 +148,10 @@ void FixedUpdate()
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
-          //if(isFalling)
+          if(isLanding)
          {
-            _playerAnim.SetBool("IsFalling", false);
-            isFalling = false; 
+            _playerAnim.SetBool("isLanding", false);
+            isLanding = false; 
 
             }
           _playerAnim.SetBool("IsGrounded", true);
@@ -124,6 +159,16 @@ void FixedUpdate()
         }
        
     }
+// nicht in Boxen rein 
+// private void OnCollisionEnter (Collision collision)
+//     {
+//         if(collision.gameObject.CompareTag("Block"))
+//         {
+//           transform.Translate(0,0,-1);
+//         }
+       
+//     }
+
 
            
 }
