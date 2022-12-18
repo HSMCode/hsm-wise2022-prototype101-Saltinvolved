0e550ffffa56e17fd30bd0f7241494a81b6dfff9
Saltinvolved <alina.grohe@live.de>
2022-12-18 16:58:30 +0100

    Score on Jump/ score doesnt go over 1

--- Assets/Scripts/Jump 203/MoveEnemy.cs
+++ Assets/Scripts/Jump 203/MoveEnemy.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Variabeles for player
  public Transform Player;
  private Rigidbody rig;
  public float speed;
     
-
+  // for score
+private GameObject _gameUI;
+private Text scoreUI;
+public string scoreText = "Score: ";
+private int currentScore = 0;
+public int addScore = 2;
+public int winScore = 6;
     void Start()
     {
         rig = this.GetComponent<Rigidbody>();
+        
+    scoreUI = GameObject.Find("Score").GetComponent<Text>();
+    _gameUI.SetActive(true); 
     }
     void FixedUpdate()
     {
@@ -24,6 +34,8 @@ public class MoveEnemy : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            currentScore += addScore;
+            scoreUI.text = scoreText + currentScore.ToString();
              Destroy (gameObject);
         }
     }
